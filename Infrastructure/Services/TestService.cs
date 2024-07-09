32673052995b2e13d498a28dc8520b5cdf30fce6
trevor.bonner <bonnert4@macewan.ca>
2024-07-09 15:54:05 -0600

    Add project files.

---
+++ Infrastructure/Services/TestService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class TestService : ITestService
+    {
+        public TestModel GetModelByName(string name)
+        {
+            var test = new TestModel()
+            {
+                Name = name
+            };
+
+            return test;
+        }
+    }
+}
---
+++ TableReservation/Controllers/MyTestController.cs
@@ -0,0 +1,49 @@
+﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TableReservation.Controllers
+{
+    [ApiController]
+    [Route("[controller]/v1")]
+    public class MyTestController : ControllerBase
+    {
+        private ITestService service;
+
+        public MyTestController(ITestService service) 
+        {
+            this.service = service;
+        }
+
+        [HttpGet("First")]
+        public IActionResult FirstTest()
+        {
+            return NoContent();
+        }
+
+        [HttpGet("Second")]
+        public IActionResult SecondTest()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("Third")]
+        public IActionResult ThirdTest(int id, string name) 
+        {
+            return Ok(id);
+        }
+
+        [HttpGet("Fourth/{id}/{name}")]
+        public IActionResult FourthTest(int id, string name)
+        {
+            return Ok(name);
+        }
+
+        [HttpGet("Fifth/{name}")]
+        public IActionResult FifthTest(string name)
+        {
+            var test = service.GetModelByName(name);
+
+            return Ok(test.Name);
+        }
+    }
+}
